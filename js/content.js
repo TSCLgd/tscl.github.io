436088652b282a295c38cdd3df0a52d015dd8abd
TSCLgd <106609328+TSCLgd@users.noreply.github.com>
2022-06-02 23:19:00 +0700

    Update content.js

--- js/content.js
+++ js/content.js
@@ -3,7 +3,7 @@ import { round, score } from './score.js';
 /**
  * Path to directory containing `_list.json` and all levels
  */
-const dir = '/tscl.github.io/json';
+const dir = '/tscl.github.io/data/json';
 
 export async function fetchList() {
     const listResult = await fetch(`${dir}/_list.json`, {
