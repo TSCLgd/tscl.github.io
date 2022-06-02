90a10dee1465e74b543b828c4837e5120d88e13b
TSCLgd <106609328+TSCLgd@users.noreply.github.com>
2022-06-02 10:37:06 +0700

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,9 @@
-import List from 'tscl.github.io/pages/List.js';
-import Leaderboard from 'tscl.github.io/pages/Leaderboard.js';
-import Roulette from 'tscl.github.io/pages/Roulette.js';
+import List from './pages/List.js';
+import Leaderboard from './pages/Leaderboard.js';
+import Roulette from '/pages/Roulette.js';
 
 export default [
-    { path: 'tscl.github.io/', component: List },
-    { path: 'tscl.github.io/leaderboard', component: Leaderboard },
-    { path: 'tscl.github.io/roulette', component: Roulette },
+    { path: './', component: List },
+    { path: './leaderboard', component: Leaderboard },
+    { path: './roulette', component: Roulette },
 ];
